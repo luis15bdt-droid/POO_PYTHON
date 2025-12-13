20e32d534f60283d310d1ecbd231e87041df8e54
Luis <luis15bdt@gmail.com>
2025-12-13 11:18:41 -0500

    Update libro.py

--- dsji/libro.py
+++ dsji/libro.py
@@ -3,7 +3,6 @@ class LbrO (PRoducto):
     def __init__(self, precio, titulo, autor, editorial, año_edicion, preferencias, genero):
         super().__init__(precio, titulo, autor, editorial, año_edicion, preferencias)
         self.genero = genero
-    def informacion(self):
-        print (f"El genero de {self.autor} es {self.genero}")
-        return super().informacion()
+
     
+
