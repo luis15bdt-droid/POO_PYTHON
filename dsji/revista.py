14fa6d1d21262f2c5351179ece9a314cac3315da
Luis <luis15bdt@gmail.com>
2025-12-13 11:24:12 -0500

    Update revista.py

--- dsji/revista.py
+++ dsji/revista.py
@@ -3,6 +3,3 @@ class REvista (PRoducto):
     def __init__(self, precio, titulo, autor, editorial, año_edicion, preferencias, categoria):
         super().__init__(precio, titulo, autor, editorial, año_edicion, preferencias)
         self.categoria = categoria 
-    def informacion(self):
-        print ("La categoria del producto es: {self.categoria}")
-        return super().informacion()
